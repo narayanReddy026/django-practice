a54fea91faf1e8b2ca37a7e22d8fb64bdad778f3
Narayan Reddy <baddam.narayan@gmail.com>
2024-08-27 18:52:10 -0400

    added admin details and connected with database

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index)
+    path('meetups/', views.index, name='all-meetups'),
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail')
 
 ]
