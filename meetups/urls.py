1c5f3d55d5cbc12716a40b92adfb50ca0a1318af
Narayan Reddy <baddam.narayan@gmail.com>
2024-08-28 16:02:41 -0400

    compleated the app

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'),
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail')
+    path('', views.index, name='all-meetups'),
+    path('<slug:meetup_slug>/success', views.conform_registration, name='conform-registration'),
+    path('<slug:meetup_slug>', views.meetup_details, name='meetup-detail')
+   
 
 ]
